db5eab3f200fbfe99503de4da22d2062b4e09a73
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2021-05-07 23:42:41 +0100

    Add initial ifttt endpoints

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,18 +1,26 @@
 const express = require('express');
-const Gpio = require('onoff').Gpio;
+const { Gpio } = require('onoff');
 
 const router = express.Router();
-const LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+const LED = new Gpio(4, 'out'); // use GPIO pin 4, and specify that it is output
 const ON = 1;
 const OFF = 0;
 
+router.get('/status', async (req, res) => {
+  res.json({ status: 'OK' });
+});
+
+router.post('/actions/switch', async (req, res) => {
+  res.json({ status: 'OK' });
+});
+
 router.get('/on', async (req, res) => {
-  LED.writeSync(ON)
+  LED.writeSync(ON);
   res.json({ status: 'ON' });
 });
 
 router.get('/off', async (req, res) => {
-  LED.writeSync(OFF)
+  LED.writeSync(OFF);
   res.json({ status: 'OFF' });
 });
 
